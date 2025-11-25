c95f9f1db4964026f3f7218bd766402051c36461
pod32g <3311662+pod32g@users.noreply.github.com>
2025-11-24 20:10:29 -0600

    Initial Commit

---
+++ OmniSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OmniSync
+//
+//  Created by David on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OmniSync/OmniSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniSyncApp.swift
+//  OmniSync
+//
+//  Created by David on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OmniSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
